9988ac42d94b7759cfcf66798d34b7783717ff5c
Neil <iforgot290@gmail.com>
2016-10-27 16:24:59 -0400

    Finished tier modifier and modified rarity to include percents

--- src/me/neildennis/crypticrpg/items/attribs/Rarity.java
+++ src/me/neildennis/crypticrpg/items/attribs/Rarity.java
@@ -1,22 +1,47 @@
 package me.neildennis.crypticrpg.items.attribs;
 
+import java.util.Random;
+
 import org.bukkit.ChatColor;
 
 public enum Rarity {
 	
-	COMMON(ChatColor.GRAY + "Common"),
-	UNCOMMON(ChatColor.GREEN + "Uncommon"),
-	RARE(ChatColor.AQUA + "Rare"),
-	UNIQUE(ChatColor.YELLOW + "Unique");
+	COMMON(ChatColor.GRAY + "Common", 0.0F, 0.60F),
+	UNCOMMON(ChatColor.GREEN + "Uncommon", 0.61F, 0.75F),
+	RARE(ChatColor.AQUA + "Rare", 0.76F, 0.90F),
+	UNIQUE(ChatColor.YELLOW + "Unique", 0.91F, 1.0F);
 	
 	private String display;
+	private float lower, upper;
 	
-	Rarity(String display){
+	Rarity(String display, float lower, float upper){
 		this.display = display;
+		this.lower = lower;
+		this.upper = upper;
 	}
 	
 	public String getDisplay(){
 		return display;
+	}
+	
+	public float getRandomPct(){
+		Random r = new Random();
+		float range = upper - lower;
+		float f = r.nextFloat();
+		float gen = f * range;
+		
+		return lower + gen;
+	}
+	
+	public static Rarity getByPct(float pct){
+		if (pct < Rarity.COMMON.upper)
+			return Rarity.COMMON;
+		else if (pct < Rarity.UNCOMMON.upper)
+			return Rarity.UNCOMMON;
+		else if (pct < Rarity.RARE.upper)
+			return Rarity.RARE;
+		else
+			return Rarity.UNIQUE;
 	}
 
 }
--- src/me/neildennis/crypticrpg/items/generator/modifiers/ItemModifier.java
+++ src/me/neildennis/crypticrpg/items/generator/modifiers/ItemModifier.java
@@ -1,7 +1,9 @@
 package me.neildennis.crypticrpg.items.generator.modifiers;
 
 import java.util.HashMap;
+import java.util.Random;
 
+import me.neildennis.crypticrpg.items.attribs.Rarity;
 import me.neildennis.crypticrpg.items.attribs.Tier;
 
 public class ItemModifier implements Comparable<ItemModifier>{
@@ -42,34 +44,58 @@ public class ItemModifier implements Comparable<ItemModifier>{
 	
 	public class TierModifier {
 		
-		private int low, high, lowHigh;
+		private int low, high, middle;
 		private ModifierType type;
 		
 		public TierModifier(ModifierType type, int low, int high){
 			this.type = type;
 			this.low = low;
 			this.high = high;
+			this.middle = high;
 		}
 		
-		public TierModifier(ModifierType type, int low, int high, int lowHigh){
+		public TierModifier(ModifierType type, int low, int high, int middle){
 			this(type, low, high);
-			this.lowHigh = lowHigh;
+			this.middle = middle;
 		}
 		
-		public int generateValue(){
+		public int[] generateValue(Rarity rarity){
+			int[] values = new int[2];
+			Random r = new Random();
 			
+			int first = (middle - low > 0 ? r.nextInt(middle - low) + low : low);
+			int second = first;
 			
-			if (type == ModifierType.STATIC){
+			if (type == ModifierType.DOUBLE){
+				second = r.nextInt(high - first) + first;
+			} else if (type == ModifierType.TRIPLE){
+				second = r.nextInt(high - middle) + middle;
+			} else if (type == ModifierType.RANGE){
+				float rareValue = 0.0F;
 				
+				if (rarity == null) rareValue = r.nextFloat();
+				else rareValue = rarity.getRandomPct();
+			
+				int range = high - low;
+				int x = (int) (range * rareValue);
+				int dps = low + x;
+				
+				float spreadRarity = r.nextFloat();
+				first = (int) (middle * spreadRarity);
+				
+				second = 2 * dps - first;
 			}
 			
-			return 0;
+			values[0] = first;
+			values[1] = second;
+			
+			return values;
 		}
 		
 	}
 	
 	public enum ModifierType{
-		STATIC, DOUBLE, TRIPLE;
+		STATIC, DOUBLE, TRIPLE, RANGE;
 	}
 
 }
