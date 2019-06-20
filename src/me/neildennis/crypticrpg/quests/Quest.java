3a0f71ad49b3817678adec025237a806f4eab04e
Neil <iforgot290@gmail.com>
2019-06-19 23:24:21 -0400

    Starting to add quests feature

---
+++ src/me/neildennis/crypticrpg/quests/Quest.java
@@ -0,0 +1,15 @@
+package me.neildennis.crypticrpg.quests;
+
+public class Quest {
+	
+	private int questId;
+	
+	public Quest(int questId) {
+		this.questId = questId;
+	}
+	
+	public int getQuestId() {
+		return questId;
+	}
+
+}
---
+++ src/me/neildennis/crypticrpg/quests/QuestManager.java
@@ -0,0 +1,22 @@
+package me.neildennis.crypticrpg.quests;
+
+import me.neildennis.crypticrpg.Manager;
+
+public class QuestManager extends Manager {
+
+	@Override
+	public void registerTasks() {
+		
+	}
+
+	@Override
+	public void onEnable() {
+		
+	}
+
+	@Override
+	public void onDisable() {
+		
+	}
+
+}
